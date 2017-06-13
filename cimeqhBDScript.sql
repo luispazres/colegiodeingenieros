04052735f0b62b55d738bd7de2ab6705c5516e14
luispazreyes <luiseduardopazreyes@hotmail.es>
2017-06-13 15:16:15 -0600

    base de datos

---
+++ cimeqhBDScript.sql
@@ -0,0 +1,356 @@
+CREATE DATABASE  IF NOT EXISTS `cimeqh` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `cimeqh`;
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: cimeqh
+-- ------------------------------------------------------
+-- Server version	5.7.14
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tblconexiones`
+--
+
+DROP TABLE IF EXISTS `tblconexiones`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tblconexiones` (
+  `conexionId` int(11) NOT NULL AUTO_INCREMENT,
+  `conexionDescripcion` varchar(100) NOT NULL,
+  PRIMARY KEY (`conexionId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tblconexiones`
+--
+
+LOCK TABLES `tblconexiones` WRITE;
+/*!40000 ALTER TABLE `tblconexiones` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tblconexiones` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tbldepartamentos`
+--
+
+DROP TABLE IF EXISTS `tbldepartamentos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tbldepartamentos` (
+  `departamentoId` int(11) NOT NULL,
+  `departamentoDescripcion` varchar(100) NOT NULL,
+  PRIMARY KEY (`departamentoId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbldepartamentos`
+--
+
+LOCK TABLES `tbldepartamentos` WRITE;
+/*!40000 ALTER TABLE `tbldepartamentos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tbldepartamentos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tbldocumentosaprobacion`
+--
+
+DROP TABLE IF EXISTS `tbldocumentosaprobacion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tbldocumentosaprobacion` (
+  `documentosAprobacionId` int(11) NOT NULL AUTO_INCREMENT,
+  `solicitudAprobacionId` int(11) NOT NULL,
+  `documentoDireccion` varchar(300) NOT NULL,
+  PRIMARY KEY (`documentosAprobacionId`),
+  KEY `solicitudAprobacionId_idx` (`solicitudAprobacionId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbldocumentosaprobacion`
+--
+
+LOCK TABLES `tbldocumentosaprobacion` WRITE;
+/*!40000 ALTER TABLE `tbldocumentosaprobacion` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tbldocumentosaprobacion` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tblestadocuenta`
+--
+
+DROP TABLE IF EXISTS `tblestadocuenta`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tblestadocuenta` (
+  `estadoCuentaId` int(11) NOT NULL AUTO_INCREMENT,
+  `estadoCuentaDescripcion` varchar(100) NOT NULL,
+  PRIMARY KEY (`estadoCuentaId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tblestadocuenta`
+--
+
+LOCK TABLES `tblestadocuenta` WRITE;
+/*!40000 ALTER TABLE `tblestadocuenta` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tblestadocuenta` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tblproyectos`
+--
+
+DROP TABLE IF EXISTS `tblproyectos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tblproyectos` (
+  `proyectoId` int(11) NOT NULL AUTO_INCREMENT,
+  `proyectoNombrePropietario` varchar(100) NOT NULL,
+  `proyectoIdentidadPropietario` varchar(30) NOT NULL,
+  `proyectoCelularPropietario` varchar(45) NOT NULL,
+  `proyectoEmailPropietario` varchar(100) NOT NULL,
+  `proyectoDireccionPropietario` varchar(100) NOT NULL,
+  `proyectoTelefonoPropietario` varchar(45) NOT NULL,
+  `departamentoId` int(11) NOT NULL,
+  `proyectoDescrpcion` varchar(100) NOT NULL,
+  `proyectoLatitud` float NOT NULL,
+  `proyectoLongitud` float NOT NULL,
+  `proyectoDireccion` varchar(200) NOT NULL,
+  `usuarioIdentidad` varchar(25) NOT NULL,
+  PRIMARY KEY (`proyectoId`),
+  KEY `usuarioIdentidad_idx` (`usuarioIdentidad`),
+  KEY `departamentoId_idx` (`departamentoId`),
+  CONSTRAINT `departamentoId` FOREIGN KEY (`departamentoId`) REFERENCES `tbldepartamentos` (`departamentoId`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `usuarioIdentidad` FOREIGN KEY (`usuarioIdentidad`) REFERENCES `tblusuarios` (`usuarioIdentidad`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tblproyectos`
+--
+
+LOCK TABLES `tblproyectos` WRITE;
+/*!40000 ALTER TABLE `tblproyectos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tblproyectos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tblroles`
+--
+
+DROP TABLE IF EXISTS `tblroles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tblroles` (
+  `rolId` int(11) NOT NULL AUTO_INCREMENT,
+  `rolDescripcion` varchar(100) NOT NULL,
+  PRIMARY KEY (`rolId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tblroles`
+--
+
+LOCK TABLES `tblroles` WRITE;
+/*!40000 ALTER TABLE `tblroles` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tblroles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tblsolicitudaprobacion`
+--
+
+DROP TABLE IF EXISTS `tblsolicitudaprobacion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tblsolicitudaprobacion` (
+  `solicitudAprobacionId` int(11) NOT NULL AUTO_INCREMENT,
+  `solicitudAaprobacionMontoEstimado` float NOT NULL,
+  `solicitudAprobacionCosto` float NOT NULL,
+  `estadoSolicitudAprobacion` int(11) NOT NULL,
+  PRIMARY KEY (`solicitudAprobacionId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tblsolicitudaprobacion`
+--
+
+LOCK TABLES `tblsolicitudaprobacion` WRITE;
+/*!40000 ALTER TABLE `tblsolicitudaprobacion` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tblsolicitudaprobacion` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tblsolicitudfactibilidad`
+--
+
+DROP TABLE IF EXISTS `tblsolicitudfactibilidad`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tblsolicitudfactibilidad` (
+  `solicitudFactibilidadId` int(11) NOT NULL AUTO_INCREMENT,
+  `voltajeId` int(11) NOT NULL,
+  `conexionId` int(11) NOT NULL,
+  `solicitudFactibilidadPotencia` int(11) NOT NULL,
+  `solicitudadFactibilidadCrecimientoEsperado` float NOT NULL,
+  `solicitudFactibilidadKva` float NOT NULL,
+  `estadoFactibilidadId` int(11) NOT NULL,
+  `proyectoId` int(11) NOT NULL,
+  PRIMARY KEY (`solicitudFactibilidadId`),
+  KEY `proyectoId_idx` (`proyectoId`),
+  KEY `voltajeId_idx` (`voltajeId`),
+  KEY `conexionID_idx` (`conexionId`),
+  CONSTRAINT `conexionID` FOREIGN KEY (`conexionId`) REFERENCES `tblconexiones` (`conexionId`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `proyectoId` FOREIGN KEY (`proyectoId`) REFERENCES `tblproyectos` (`proyectoId`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `voltajeId` FOREIGN KEY (`voltajeId`) REFERENCES `tblvoltajes` (`voltajeId`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tblsolicitudfactibilidad`
+--
+
+LOCK TABLES `tblsolicitudfactibilidad` WRITE;
+/*!40000 ALTER TABLE `tblsolicitudfactibilidad` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tblsolicitudfactibilidad` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tblsolicitudrecpcion`
+--
+
+DROP TABLE IF EXISTS `tblsolicitudrecpcion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tblsolicitudrecpcion` (
+  `solicitudRecepcioId` int(11) NOT NULL AUTO_INCREMENT,
+  `solicitudRecepcionCosto` float NOT NULL,
+  `solicitudRecepcioEstado` int(11) NOT NULL,
+  `solicitudAprobacionId` int(11) NOT NULL,
+  PRIMARY KEY (`solicitudRecepcioId`),
+  KEY `fk_tblSolicitudRecpcion_tblsolicitudaprobacion1_idx` (`solicitudAprobacionId`),
+  CONSTRAINT `fk_tblSolicitudRecpcion_tblsolicitudaprobacion1` FOREIGN KEY (`solicitudAprobacionId`) REFERENCES `tblsolicitudaprobacion` (`solicitudAprobacionId`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tblsolicitudrecpcion`
+--
+
+LOCK TABLES `tblsolicitudrecpcion` WRITE;
+/*!40000 ALTER TABLE `tblsolicitudrecpcion` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tblsolicitudrecpcion` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tbltimbres`
+--
+
+DROP TABLE IF EXISTS `tbltimbres`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tbltimbres` (
+  `timbreId` int(11) NOT NULL AUTO_INCREMENT,
+  `solicitudAprobacionId` int(11) NOT NULL,
+  PRIMARY KEY (`timbreId`),
+  KEY `solicitudAprobacionId_idx` (`solicitudAprobacionId`),
+  CONSTRAINT `solicitudAprobacionId` FOREIGN KEY (`solicitudAprobacionId`) REFERENCES `tblsolicitudaprobacion` (`solicitudAprobacionId`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbltimbres`
+--
+
+LOCK TABLES `tbltimbres` WRITE;
+/*!40000 ALTER TABLE `tbltimbres` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tbltimbres` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tblusuarios`
+--
+
+DROP TABLE IF EXISTS `tblusuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tblusuarios` (
+  `usuarioIdentidad` varchar(25) NOT NULL,
+  `usuarioPrimerNombre` varchar(45) NOT NULL,
+  `usuarioSegundoNombre` varchar(45) NOT NULL,
+  `usuarioPrimerApellido` varchar(45) NOT NULL,
+  `usuarioSegundoApellido` varchar(45) NOT NULL,
+  `usuarioNumeroColegiacion` varchar(45) NOT NULL,
+  `usuarioCelular` varchar(45) NOT NULL,
+  `usuarioTelefono` varchar(45) NOT NULL,
+  `usuarioDireccion` varchar(45) NOT NULL,
+  `usuarioContrasenia` varchar(45) NOT NULL,
+  `estadoCuentaId` int(11) NOT NULL,
+  `rolId` int(11) NOT NULL,
+  PRIMARY KEY (`usuarioIdentidad`),
+  KEY `rolId_idx` (`rolId`),
+  KEY `estadoCuentaId_idx` (`estadoCuentaId`),
+  CONSTRAINT `estadoCuentaId` FOREIGN KEY (`estadoCuentaId`) REFERENCES `tblestadocuenta` (`estadoCuentaId`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `rolId` FOREIGN KEY (`rolId`) REFERENCES `tblroles` (`rolId`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tblusuarios`
+--
+
+LOCK TABLES `tblusuarios` WRITE;
+/*!40000 ALTER TABLE `tblusuarios` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tblusuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tblvoltajes`
+--
+
+DROP TABLE IF EXISTS `tblvoltajes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tblvoltajes` (
+  `voltajeId` int(11) NOT NULL AUTO_INCREMENT,
+  `voltajeDescripcion` varchar(100) NOT NULL,
+  PRIMARY KEY (`voltajeId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tblvoltajes`
+--
+
+LOCK TABLES `tblvoltajes` WRITE;
+/*!40000 ALTER TABLE `tblvoltajes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tblvoltajes` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-06-13 15:15:14
